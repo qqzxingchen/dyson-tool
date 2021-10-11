0a6735cd22bb90bb2a24d6511365c16902bab77a
guopan15 <guopan15>
2021-10-12 00:09:50 +0800

    增加小型运载火箭的配方

--- src/main/java/org/gp/FormulaService.java
+++ src/main/java/org/gp/FormulaService.java
@@ -3,9 +3,7 @@ package org.gp;
 import org.gp.formula.AbsFormulaManager;
 import org.gp.formula.impl.FormulaV0_8_22;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FormulaService {
 
@@ -14,7 +12,7 @@ public class FormulaService {
 
         Map<String, Double> costMap = manager.calcCost(
                 new HashMap<String, Double>() {{
-                    put(targetEntity, 1.0);
+                    put(targetEntity, 60.0);
                 }},
                 Arrays.asList( specialEntity )
         );
@@ -38,8 +36,18 @@ public class FormulaService {
 
 
     private static void printCost(Map<String, Double> costMap){
-        for ( String key : costMap.keySet() ){
-            System.out.println( String.format("%s: \t%s",key,costMap.get( key )) );
+        ArrayList<Map.Entry<String, Double>> list = new ArrayList<>(costMap.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String,Double>>() {
+            @Override
+            public int compare(Map.Entry<String,Double> o1, Map.Entry<String,Double> o2) {
+                return o1.getValue().compareTo( o2.getValue() );
+            }
+        });
+
+
+
+        for ( Map.Entry<String,Double> entry : list ){
+            System.out.println( String.format("%s: \t%s",entry.getKey(),entry.getValue()) );
         }
     }
 }
--- src/main/java/org/gp/formula/impl/FormulaV0_8_22.java
+++ src/main/java/org/gp/formula/impl/FormulaV0_8_22.java
@@ -13,6 +13,10 @@ public class FormulaV0_8_22 extends AbsFormulaManager {
         List<DysonFormula> formulaList = new ArrayList<>();
         formulaList.addAll( genFinal() );
         formulaList.addAll( genMid() );
+        formulaList.addAll( gen_碳纳米管_2() );
+        formulaList.addAll( gen_光子合并器_1() );
+        formulaList.addAll( gen_卡西米尔晶体_1() );
+        formulaList.addAll( genLow() );
         return formulaList;
     }
 
@@ -25,19 +29,248 @@ public class FormulaV0_8_22 extends AbsFormulaManager {
                 G(4,"氘核燃料棒"),
                 G(2,"量子芯片")
         ) );
+        formulaList.add( G(
+                G(2,"氘核燃料棒"),
+                12,
+                G(1,"钛合金"),
+                G(20,"重氢"),
+                G(1,"超级磁场环")
+        ) );
         return formulaList;
     }
 
     private static List<DysonFormula> genMid(){
         List<DysonFormula> formulaList = new ArrayList<>();
         formulaList.add( G(
-                G(2,"塑料"),
-                1,
-                G(1,"煤"),
-                G(20,"石油气")
+                G(1,"戴森球组件"),
+                8,
+                G(3,"框架材料"),
+                G(3,"太阳帆"),
+                G(3,"处理器")
+        ) );
+        formulaList.add( G(
+                G(1,"量子芯片"),
+                6,
+                G(2,"处理器"),
+                G(2,"位面过滤器")
+        ) );
+
+        formulaList.add( G(
+                G(4,"钛合金"),
+                12,
+                G(4,"钛块"),
+                G(4,"钢材"),
+                G(8,"硫酸")
+        ) );
+        formulaList.add( G(
+                G(1,"钢材"),
+                3,
+                G(3,"铁块")
+        ) );
+        formulaList.add( G(
+                G(1,"超级磁场环"),
+                3,
+                G(2,"电磁涡轮"),
+                G(3,"磁铁"),
+                G(1,"高能石墨")
+        ) );
+
+        formulaList.add( G(
+                G(1,"框架材料"),
+                6,
+                G(4,"碳纳米管"),
+                G(1,"钛合金"),
+                G(1,"高纯硅块")
+        ) );
+
+        formulaList.add( G(
+                G(2,"太阳帆"),
+                4,
+                G(1,"石墨烯"),
+                G(1,"光子合并器")
+        ) );
+
+        formulaList.add( G(
+                G(1,"位面过滤器"),
+                12,
+                G(1,"卡西米尔晶体"),
+                G(2,"钛化玻璃")
+        ) );
+
+        formulaList.add( G(
+                G(2,"钛化玻璃"),
+                5,
+                G(2,"玻璃"),
+                G(2,"钛块"),
+                G(2,"水")
         ) );
         return formulaList;
     }
+
+
+
+
+
+    // 碳纳米管
+    private static List<DysonFormula> gen_碳纳米管_1() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(2,"碳纳米管"),
+                4,
+                G(3,"石墨烯"),
+                G(1,"钛块")
+        ) );
+        return formulaList;
+    }
+    private static List<DysonFormula> gen_碳纳米管_2() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(2,"碳纳米管"),
+                4,
+                G(2,"刺笋结晶")
+        ) );
+        return formulaList;
+    }
+
+
+
+
+    // 光子合并器
+    private static List<DysonFormula> gen_光子合并器_1() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(1,"光子合并器"),
+                3,
+                G(2,"棱镜"),
+                G(1,"电路板")
+        ) );
+        return formulaList;
+    }
+    private static List<DysonFormula> gen_光子合并器_2() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(1,"光子合并器"),
+                3,
+                G(1,"光栅石"),
+                G(1,"电路板")
+        ) );
+        return formulaList;
+    }
+
+
+
+    // 卡西米尔晶体
+    private static List<DysonFormula> gen_卡西米尔晶体_1() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(1,"卡西米尔晶体"),
+                4,
+                G(1,"钛晶石"),
+                G(2,"石墨烯"),
+                G(12,"氢气")
+        ) );
+        return formulaList;
+    }
+    private static List<DysonFormula> gen_卡西米尔晶体_2() {
+        List<DysonFormula> formulaList = new ArrayList<>();
+        formulaList.add( G(
+                G(1,"卡西米尔晶体"),
+                4,
+                G(4,"光栅石"),
+                G(2,"石墨烯"),
+                G(12,"氢气")
+        ) );
+        return formulaList;
+    }
+
+
+
+
+
+    private static List<DysonFormula> genLow(){
+        List<DysonFormula> formulaList = new ArrayList<>();
+
+        // 处理器
+        formulaList.add( G(
+                G(1,"处理器"),
+                3,
+                G(2,"电路板"),
+                G(2,"微晶元件")
+        ) );
+        formulaList.add( G(
+                G(2,"电路板"),
+                1,
+                G(2,"铁块"),
+                G(1,"铜块")
+        ) );
+        formulaList.add( G(
+                G(1,"微晶元件"),
+                2,
+                G(2,"高纯硅块"),
+                G(1,"铜块")
+        ) );
+
+        // 电磁涡轮
+        formulaList.add( G(
+                G(1,"电磁涡轮"),
+                2,
+                G(2,"电动机"),
+                G(2,"磁线圈")
+        ) );
+        formulaList.add( G(
+                G(1,"电动机"),
+                2,
+                G(2,"铁块"),
+                G(1,"齿轮"),
+                G(1,"磁线圈")
+        ) );
+        formulaList.add( G(
+                G(1,"齿轮"),
+                1,
+                G(1,"铁块")
+        ) );
+        formulaList.add( G(
+                G(2,"磁线圈"),
+                1,
+                G(2,"磁铁"),
+                G(1,"铜块")
+        ) );
+
+        // 棱镜
+        formulaList.add( G(
+                G(2,"棱镜"),
+                2,
+                G(3,"玻璃")
+        ) );
+        formulaList.add( G(
+                G(1,"玻璃"),
+                2,
+                G(2,"石矿")
+        ) );
+
+        // 钛晶石
+        formulaList.add( G(
+                G(1,"钛晶石"),
+                4,
+                G(1,"有机晶体"),
+                G(3,"钛块")
+        ) );
+
+        return formulaList;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
